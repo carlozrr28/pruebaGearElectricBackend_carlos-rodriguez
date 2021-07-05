531b4b9b77fc3bb7195bb56755d4033d2559d34a
carlozrr28 <59266297+carlozrr28@users.noreply.github.com>
2021-07-05 00:11:56 -0500

    Add files via upload
    versión de prueba 1

---
+++ Registro_Prueba/edit.php
@@ -0,0 +1,136 @@
+<?php
+include("conexion.php");
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<!--
+Project      : Datos de empleados con PHP, MySQLi y Bootstrap CRUD  (Create, read, Update, Delete) 
+Author		 :  Carlos A. Rodriguez Ruiz
+Email	 	 : corlos347@gmail.com
+-->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Datos de empleados</title>
+
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-datepicker.css" rel="stylesheet">
+	<link href="css/style_nav.css" rel="stylesheet">
+	<style>
+		.content {
+			margin-top: 80px;
+		}
+	</style>
+	
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<?php include("nav.php");?>
+	</nav>
+	<div class="container">
+		<div class="content">
+			<h2>Datos del empleados &raquo; Editar datos</h2>
+			<hr />
+			
+			<?php
+			// escaping, additionally removing everything that could be (html/javascript-) code
+			$nik = mysqli_real_escape_string($con,(strip_tags($_GET["nik"],ENT_QUOTES)));
+			$sql = mysqli_query($con, "SELECT * FROM usuarios WHERE Codigo='$nik'");
+			if(mysqli_num_rows($sql) == 0){
+				header("Location: index.php");
+			}else{
+				$row = mysqli_fetch_assoc($sql);
+			}
+			if(isset($_POST['save'])){
+				$Codigo		     = mysqli_real_escape_string($con,(strip_tags($_POST["Codigo"],ENT_QUOTES)));//Escanpando caracteres 
+				$nombres		     = mysqli_real_escape_string($con,(strip_tags($_POST["Nombre"],ENT_QUOTES)));//Escanpando caracteres 
+				$Tipo_Docu	 = mysqli_real_escape_string($con,(strip_tags($_POST["Tipo_Docu"],ENT_QUOTES)));//Escanpando caracteres 
+				$Documento	 = mysqli_real_escape_string($con,(strip_tags($_POST["Documento"],ENT_QUOTES)));//Escanpando caracteres 
+				$telefono		 = mysqli_real_escape_string($con,(strip_tags($_POST["telefono"],ENT_QUOTES)));//Escanpando caracteres 
+				$Correo	     = mysqli_real_escape_string($con,(strip_tags($_POST["Correo"],ENT_QUOTES)));//Escanpando caracteres 
+				$estado			 = mysqli_real_escape_string($con,(strip_tags($_POST["estado"],ENT_QUOTES)));//Escanpando caracteres  
+				
+				$update = mysqli_query($con, "UPDATE USUARIOS SET Nombre='$nombres', Tipo_Docu='$Tipo_Docu', Telefono='$telefono', Correo='$Correo', estado='$estado' WHERE Codigo='$nik'") or die(mysqli_error());
+				if($update){
+					header("Location: edit.php?nik=".$nik."&pesan=sukses");
+				}else{
+					echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Error, no se pudo guardar los datos.</div>';
+				}
+			}
+			
+			if(isset($_GET['pesan']) == 'sukses'){
+				echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Los datos han sido guardados con éxito.</div>';
+			}
+			?>
+			<form class="form-horizontal" action="" method="post">
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Código</label>
+					<div class="col-sm-2">
+						<input type="text" name="Codigo" value="<?php echo $row ['Codigo']; ?>" class="form-control" placeholder="NIK" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Nombres</label>
+					<div class="col-sm-4">
+						<input type="text" name="Nombre" value="<?php echo $row ['Nombre']; ?>" class="form-control" placeholder="Nombres" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Tipo de Documento</label>
+					<div class="col-sm-4">
+						<input type="text" name="Tipo_Docu" value="<?php echo $row ['Tipo_Docu']; ?>" class="form-control" placeholder="Tipo de Documentoo" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Documento</label>
+					<div class="col-sm-4">
+						<input type="text" name="Documento" value="<?php echo $row ['Documento']; ?>" class="form-control" placeholder="Documento" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Telefono</label>
+					<div class="col-sm-3">
+					<input type="text" name="telefono" value="<?php echo $row ['Telefono']; ?>" class="form-control" placeholder="Teléfono" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Correo</label>
+					<div class="col-sm-3">
+						<input type="text" name="Correo" value="<?php echo $row ['Correo']; ?>" class="form-control" placeholder="Teléfono" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Estado</label>
+					<div class="col-sm-3">
+						<select name="estado" class="form-control">
+							<option value="">- Selecciona estado -</option>
+                            <option value="1" <?php if ($row ['Estado']==1){echo "selected";} ?>>Asistira</option>
+							<option value="2" <?php if ($row ['Estado']==2){echo "selected";} ?>>Probablemente</option>
+							<option value="3" <?php if ($row ['Estado']==3){echo "selected";} ?>>No asitire</option>
+						</select> 
+					</div>
+                   
+                </div>
+			
+				<div class="form-group">
+					<label class="col-sm-3 control-label">&nbsp;</label>
+					<div class="col-sm-6">
+						<input type="submit" name="save" class="btn btn-sm btn-primary" value="Guardar datos">
+						<a href="index.php" class="btn btn-sm btn-danger">Cancelar</a>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+</body>
+</html>
---
+++ Registro_Prueba/index.php
@@ -0,0 +1,126 @@
+<?php
+include("conexion.php");
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Datos de empleados</title>
+
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style_nav.css" rel="stylesheet">
+
+	<style>
+		.content {
+			margin-top: 80px;
+		}
+	</style>
+
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<?php include('nav.php');?>
+	</nav>
+	<div class="container">
+		<div class="content">
+			<h2>Lista de empleados</h2>
+			<hr />
+
+			<?php
+			if(isset($_GET['aksi']) == 'delete'){
+				// escaping, additionally removing everything that could be (html/javascript-) code
+				$nik = mysqli_real_escape_string($con,(strip_tags($_GET["nik"],ENT_QUOTES)));
+				$cek = mysqli_query($con, "SELECT * FROM usuarios WHERE Codigo='$nik'");
+				if(mysqli_num_rows($cek) == 0){
+					echo '<div class="alert alert-info alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> No se encontraron datos.</div>';
+				}else{
+					$delete = mysqli_query($con, "DELETE FROM usuarios WHERE Codigo='$nik'");
+					if($delete){
+						echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> Datos eliminado correctamente.</div>';
+					}else{
+						echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> Error, no se pudo eliminar los datos.</div>';
+					}
+				}
+			}
+			?>
+
+			<form class="form-inline" method="get">
+				<div class="form-group">
+					<select name="filter" class="form-control" onchange="form.submit()">
+						<option value="0">Filtros de datos de empleados</option>
+						<?php $filter = (isset($_GET['filter']) ? strtolower($_GET['filter']) : NULL);  ?>
+						<option value="1" <?php if($filter == '1'){ echo 'selected'; } ?>>Fijo</option>
+						<option value="2" <?php if($filter == '2'){ echo 'selected'; } ?>>Contratado</option>
+                        <option value="3" <?php if($filter == '3'){ echo 'selected'; } ?>>Outsourcing</option>
+					</select>
+				</div>
+			</form>
+			<br />
+			<div class="table-responsive">
+			<table class="table table-striped table-hover">
+				<tr>
+                    <th>No</th>
+					<th>Nombre</th>
+                    <th>Tipo de Documento</	th>
+					<th>Documento</th>
+					<th>Teléfono</th>
+					<th>Correo</th>
+					<th>Estado</th>
+                    <th>Acciones</th>
+				</tr>
+				<?php
+				if($filter){
+					$sql = mysqli_query($con, "SELECT * FROM usuarios WHERE estados='$filter' ORDER BY codigo ASC");
+				}else{
+					$sql = mysqli_query($con, "SELECT * FROM usuarios ORDER BY codigo ASC");
+				}
+				if(mysqli_num_rows($sql) == 0){
+					echo '<tr><td colspan="8">No hay datos.</td></tr>';
+				}else{
+					$no = 1;
+					while($row = mysqli_fetch_assoc($sql)){
+						echo '
+						<tr>
+							<td>'.$no.'</td>
+							<td><a href="profile.php?nik='.$row['Codigo'].'"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> '.$row['Nombre'].'</a></td>
+                            <td>'.$row['Tipo_Docu'].'</td>
+                            <td>'.$row['Documento'].'</td>
+							<td>'.$row['Telefono'].'</td>
+                            <td>'.$row['Correo'].'</td>
+							<td>';
+							if($row['Estado'] == '1'){
+								echo '<span class="label label-success">Fijo</span>';
+							}
+                            else if ($row['Estado'] == '2' ){
+								echo '<span class="label label-info">Contratado</span>';
+							}
+                            else if ($row['Estado'] == '3' ){
+								echo '<span class="label label-warning">Outsourcing</span>';
+							}
+						echo '
+							</td>
+							<td>
+
+								<a href="edit.php?nik='.$row['Codigo'].'" title="Editar datos" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
+								<a href="index.php?aksi=delete&nik='.$row['Codigo'].'" title="Eliminar" onclick="return confirm(\'Esta seguro de borrar los datos '.$row['Nombre'].'?\')" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
+							</td>
+						</tr>
+						';
+						$no++;
+					}
+				}
+				?>
+			</table>
+			</div>
+		</div>
+	</div><center>
+	<p>&copy; Sistemas Web <?php echo date("Y");?></p
+		</center>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
